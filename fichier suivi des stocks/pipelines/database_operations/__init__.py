5baa4ac15f9c9c695e9a1519c8287ca464efff86
Arnaud ANOBLE <156622417+aanoble@users.noreply.github.com>
2025-07-18 11:39:14 +0000

    Delete fichier suivi des stocks directory

--- fichier suivi des stocks/pipelines/database_operations/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import stock_sync_manager
-from .process_statut_prod import process_statut_prod
-
-__all__ = ["stock_sync_manager", "process_statut_prod"]
--- fichier suivi des stocks/pipelines/export_file_to_google_drive/__init__.py
+++
@@ -1,3 +0,0 @@
-from .upload_file_to_drive import upload_and_return_link
-
-__all__ = ["upload_and_return_link"]
